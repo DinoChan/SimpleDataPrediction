e1f3329587e5e3c0a25b0eb1c090f6b0b95cb3a2
DinoChan <dino.chan@live.com>
2020-07-17 10:28:40 +0800

    添加项目文件。

---
+++ SimpleDataPrediction/Forecaster.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SimpleDataPrediction
+{
+    public class Forecaster
+    {
+        public double[] GetPredictData(int forecastingPoints, double[] points)
+        {
+            var tempChart = new Chart();
+
+            tempChart.ChartAreas.Add(new ChartArea());
+            tempChart.ChartAreas[0].AxisX = new Axis();
+            tempChart.ChartAreas[0].AxisY = new Axis();
+            tempChart.Series.Add(new Series());
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                tempChart.Series[0].Points.AddXY(i, points[i]);
+            }
+
+            var trendSeries = new Series();
+            tempChart.Series.Add(trendSeries);
+
+            var typeRegression = "Exponential";
+            var forecasting = forecastingPoints.ToString();
+            var error = "false";
+            var forecastingError = "false";
+            var parameters = typeRegression + ',' + forecasting + ',' + error + ',' + forecastingError;
+
+            tempChart.DataManipulator.FinancialFormula(FinancialFormula.Forecasting, parameters, tempChart.Series[0], trendSeries);
+
+            var result = new List<double>();
+            for (int i = 0; i < trendSeries.Points.Count; i++)
+            {
+                result.Add(trendSeries.Points[i].YValues[0]);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ SimpleDataPrediction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var source = "5.857486213 6.882938361 5.938018732 7.418233591 8.102131936 10.02132021 11.30071133 13.07283147 15.03172119 17.62765193 20.33497935 23.3258717 26.49699319 29.9783162 32.92265494 35.10837941 36.3501758 36.53764154 34.1757566 30.89822951 26.63771337 22.32763029 18.38308201 14.90463905 12.35598901 9.741861183 7.720235644 6.033749358 4.565486063 3.445948379 2.494921555 1.82715694 1.368045582 0.965480591 0.722276257 0.577920119 0.448848566 0.361900586 0.29215714 0.242289617 0.202792727 0.166097863 0.069075582 0.002831501 0.000875038 0.043841538";
+
+                var values = source.Split(' ').Skip(8).Take(6).Select(t => double.Parse(t)).Reverse();
+
+                var range = Enumerable.Range(1, 6).Select(r => (double)r);
+                var forecaster = new Forecaster();
+                var result = forecaster.GetPredictData(8, values.ToArray());
+                Console.WriteLine("Analysis completed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
